ab68d02168b21efb137d3f69dd71678dec0315c4
Sergii Gama <balls@sergiigama.com>
2018-06-19 12:38:18 +0300

    Add CRUD for `Address` model

---
+++ views/address/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Addresses');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="address-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php Pjax::begin(); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Address'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'company_id',
+            'name',
+            'address1',
+            'address2',
+            //'city',
+            //'state',
+            //'country',
+            //'postalcode',
+            //'created_on',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
+</div>
