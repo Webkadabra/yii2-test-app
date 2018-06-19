2d9e85917b01ff3fdd07930ebd5026ce21e5becc
Sergii Gama <balls@sergiigama.com>
2018-06-19 12:50:43 +0300

    Add `AddressSearch` model

---
+++ models/AddressSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Address;
+
+/**
+ * AddressSearch represents the model behind the search form of `app\models\Address`.
+ */
+class AddressSearch extends Address
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'company_id'], 'integer'],
+            [['name', 'address1', 'address2', 'city', 'state', 'country', 'postalcode', 'created_on'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Address::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'company_id' => $this->company_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'address1', $this->address1])
+            ->andFilterWhere(['like', 'address2', $this->address2])
+            ->andFilterWhere(['like', 'city', $this->city])
+            ->andFilterWhere(['like', 'state', $this->state])
+            ->andFilterWhere(['like', 'country', $this->country])
+            ->andFilterWhere(['like', 'postalcode', $this->postalcode])
+            ->andFilterWhere(['like', 'created_on', $this->created_on]);
+
+        return $dataProvider;
+    }
+}
