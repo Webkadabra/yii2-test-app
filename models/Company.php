d611d3fe94c088609dc0f88f6ee0c271725cac46
Sergii Gama <balls@sergiigama.com>
2018-06-19 12:28:47 +0300

    Add `Company` model

---
+++ models/Company.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $country
+ *
+ * @property Address[] $addresses
+ */
+class Company extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'company';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'country'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'country' => 'Country',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAddresses()
+    {
+        return $this->hasMany(Address::className(), ['company_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CompanyQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CompanyQuery(get_called_class());
+    }
+}
