07c96f1004fa649d16f801c4797c14c055908706
Sergii Gama <balls@sergiigama.com>
2018-06-19 11:54:15 +0300

    Remove dummy `User` model to be able to use full-featured `user` module

--- config/web.php
+++ config/web.php
@@ -13,16 +13,15 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'OfD4MEvCL__Ps02Hdr2uyUXMKsO886v9',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
